fa968daacf323885b0ea5cf6dab9cc14bf3fb48d
Diego Alcocer <diegoaalcocer@gmail.com>
2018-01-05 21:21:58 +0000

    Added devise to create views and installed bootstrap to style login pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :events
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'new', to: 'events#new'
   get 'create', to: 'events#create'
   get 'index', to: 'events#index'
+  
+  root to: "events#index"
 end
 
